261830b24d32e59eb8f313f20c6c2ae87cc9bc35
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-25 15:40:46 +0200

    5th adding accordion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,57 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>@yield('title' , 'app')</title>
+@section('title' , 'index')
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+@section('content')
+<div class="container">
+<div class="accordion" id="accordionExample">
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne">
+            <button
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+            >
+                Trains
+            </button>
+        </h2>
+        <div
+            id="collapseOne"
+            class="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+        >
+            <div class="accordion-body">
+                <p>Filter by country
+                <select 
+                class="form-select mt-1"
+                id="country_select">
+                    <option value="italy">Italy</option>
+                    <option value="austria">Austria</option>
+                    <option value="netherlands">Netherlands</option>
+                </select>
+                
+                </p>
+            </div>
+            <div class="accordion-body">
+                <p>Filter by company
+                <select
+                class="form-select mt-1"
+                id="company_select">
+                    <option value="trenord">Trenord</option>
+                    <option value="mediterranea">Mediterranea</option>
+                    <option value="wiener_linen">Wiener Linen</option>
+                    <option value="ns_international">NS International</option>
+                </select>
+                </p>
+            </div>
+        </div>
+    </div>
+    
+</div>
+</div>
 
-       @vite(['resources/css/app.css' , 'resources/js/app.js'])
-    </head>
-    <body>
-    <header>
-    @include('components.header')
-    </header>
-    <main>
-    @yield('content' , 'main_content')
-    </main>
-    <footer>
-    @include('components.footer')
-    </footer>
-    </body>
-</html>
+@endsection
