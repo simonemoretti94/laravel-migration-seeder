a960289d4149dd818d0f1d3189de4d97942633bf
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-25 18:52:55 +0200

    7th trouble with blade html emmet brilliantly resolved

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,30 +2,22 @@
     <div class="row">
         <img id='train_logo' src="{{ Vite::asset('resources/img/train-logo.png') }}" alt="train_logo">
         <h3 id="title">laravel-migration-seeder</h3>
-    </div> 
+    </div>
 </div>
-<nav
-    class="nav justify-content-start  "
->
-    <a href="{{route('home')}}"
-    class="
+<nav class="nav justify-content-start  ">
+    <a href="{{route('home')}}" class="
     nav-link
-    {{Route::currentRouteName() == 'home' ? 'd-none' : ''}}"
-    >Home</a
-    >
-    <a 
-    href="{{route('all')}}"
-    class="
+    {{Route::currentRouteName() == 'home' ? 'd-none' : ''}}">Home</a>
+    <a href="{{route('all')}}" class="
     nav-link
     {{-- {{Route::currentRouteName() == 'all' ? 'd-none' : ''}} --}}
     {{Route::currentRouteName() == 'all' ? 'a_active' : ''}}
-    "
-    >Trains</a>
-    <a href="{{route('contacts')}}"
-    class="
+    ">Trains</a>
+    <a href="{{route('contacts')}}" class="
     nav-link
     {{-- {{Route::currentRouteName() == 'contacts' ? 'd-none' : ''}} --}}
     {{Route::currentRouteName() == 'contacts' ? 'a_active' : ''}}
-    "
-    >Contacts</a>
+    ">Contacts</a>
 </nav>
+
+{{-- settings->file associations->edit in settings.json --}}
