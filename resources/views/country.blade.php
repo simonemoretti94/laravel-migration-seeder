c0e8efaeba6e2739b954b2746a3f6c7353175ca5
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-26 10:04:28 +0200

    8.1th styling

--- resources/views/country.blade.php
+++ resources/views/country.blade.php
@@ -5,13 +5,16 @@
 @section('content')
     <div class="container" id="country_container">
     <h2 id="country">{{$id}}</h2>
-        <div class="row" id="country_row">
+        <div class="row flex-column" id="country_row">
 @foreach ($trains as $train)
-            <div class="col-12 p-2 text-white d-flex">
-                <p class="col-6">
-                    <span class="text-primary">Operated by: </span>
-                    {{$train['company']}}
-                </p>
+            <div class="col-12 p-2 text-white d-flex border border-1 rounded-2">
+                <div class="col-6 d-flex flex-column">
+                    <p class="col-12 text-primary">Operated by: <span class="text-white">{{$train['company']}}</span></p>
+                    <p class="col-12 text-primary text-smal">
+                        Departure: <span class="text-white">{{$train['departure_station']}}</span> <span class="text-success">|</span>
+                        Arrival: <span class="text-white">{{$train['arrival_station']}}</span>
+                    </p>
+                </div>
                 <div class="col-6 d-flex justify-content-evenly  text-center">
                     <div class="col-3 d-flex flex-column">
                         <p class="col-12">{{$train['departure_station']}}</p>
