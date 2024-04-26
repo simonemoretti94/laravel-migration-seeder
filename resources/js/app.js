fea4362926026b50b10a8f2d1894246eb419c2b9
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-26 13:46:16 +0200

    10.1th now main selector works in responsive modality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
 
+/* viewport > 450px  */
+
 const countrySelect = document.getElementById('country_select');
 
 const companySelect = document.getElementById('company_select');
@@ -15,6 +17,28 @@ countrySelect.addEventListener('change', function (e) {
 });
 
 companySelect.addEventListener('change', function (e) {
+    console.log(e.target.value);
+    //open(`/company/${e.target.value}`);
+    if (e.target.value) {
+        window.location.href = `/company/${e.target.value}`;
+    }
+});
+
+/* viewport < 450px */
+
+const countrySelect2 = document.getElementById('country_select_2');
+
+const companySelect2 = document.getElementById('company_select_2');
+
+countrySelect2.addEventListener('change', function (e) {
+    console.log(e.target.value);
+    //open(`/country/${e.target.value}`);
+    if (e.target.value) {
+        window.location.href = `/country/${e.target.value}`;
+    }
+});
+
+companySelect2.addEventListener('change', function (e) {
     console.log(e.target.value);
     //open(`/company/${e.target.value}`);
     if (e.target.value) {
