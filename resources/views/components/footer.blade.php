81a9146bbf27c14c74e1a363ba4da07f09196813
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-26 14:02:37 +0200

    10.3.1th

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,13 @@
 <div class="container"></div>
     <div class="row text-center">
         <div class="col-sm-12 col-md-6 col-lg-3 col-xl-6 col-xxl-6">
-        <ul class="list-unstyled d-flex flex-sm-row flex-md-column text-sm-left text-md-left justify-content-evenly ">
-            <li class="text-white text-capitalize">lorem1</li>
-            <li class="text-white text-capitalize">lorem1</li>
-            <li class="text-white text-capitalize">lorem1</li>
-            <li class="text-white text-capitalize">lorem1</li>
-            <li class="text-white text-capitalize">lorem1</li>
-            <li class="text-white text-capitalize">lorem1</li>
+        <ul class="list-unstyled d-flex flex-wrap flex-sm-row flex-md-column text-center text-lg-center justify-content-evenly align-items-center">
+            <li class="text-white text-capitalize col-sm-4">lorem1</li>
+            <li class="text-white text-capitalize col-sm-4">lorem1</li>
+            <li class="text-white text-capitalize col-sm-4">lorem1</li>
+            <li class="text-white text-capitalize col-sm-4">lorem1</li>
+            <li class="text-white text-capitalize col-sm-4">lorem1</li>
+            <li class="text-white text-capitalize col-sm-4">lorem1</li>
         </ul>
         </div>
         <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">Eccolo</div>
