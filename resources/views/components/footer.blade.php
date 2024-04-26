0fa6ffbe8d2a4f1254ad60cae93a662addfe6580
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-26 14:20:54 +0200

    11th added contacts page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container"></div>
-    <div class="row text-center">
+    <div class="row justify-content-evenly">
         <div class="col-sm-12 col-md-6 col-lg-3 col-xl-6 col-xxl-6">
         <ul class="list-unstyled d-flex flex-wrap flex-sm-row flex-md-column text-center text-lg-center justify-content-evenly align-items-center">
             <li class="text-white text-capitalize col-sm-4">lorem1</li>
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,5 +3,45 @@
 @section('title' , 'contacts')
 
 @section('content')
-contacts
+<div class="container text-white">
+    <h1 class=" text-center text-primary">Contact Us</h1>
+    <h6 class="text-primary">Feedback and Contact</h6>
+    <p>
+    Have you received exceptional service from one of our staff members? Do you have suggestions on how we can further improve our service? We value both positive feedback and constructive criticism.
+    </p>
+
+    <h6 class="text-primary">
+    How to Reach Us:
+    </h6>
+
+    <p class="mt-2">
+    <span class="text-success">
+    Phone: 
+    </span>
+    You can call us at 03153 0947 036232. Our Contact Centre team is available Monday to Friday, from 09:00 to 17:00.
+    </p>
+    <p class="mt-1">
+    <span class="text-success">
+    Email:  
+    </span>
+    Share your feedback via email at <a href="{{route('home')}}">simone.moretti.944@gmail.com</a>.
+    </p>
+    <p class="mt-1">
+    <span class="text-success">
+    Online Form: 
+    </span>
+    Fill out our online feedback form by clicking the button below.
+    Provide Feedback
+    </p>
+
+    <h4 class="text-primary">Address:</h4>
+
+    Simone Moretti
+    1st Floor, Premier House
+    Argyle Way
+    Stevenage, Hertfordshire
+    SG1 2AD
+
+    We hope our customers find this information helpful for sharing their experiences and suggestions with us. Thank you for your request! 🚂📞📧
+</div>
 @endsection
