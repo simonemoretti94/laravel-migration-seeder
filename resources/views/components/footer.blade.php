ec78e696bb8b9c42310357e0b3192f2ae8bf1179
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-27 18:40:49 +0200

    14.1th

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="container"></div>
+{{-- <div class="container"></div>
     <div class="row justify-content-evenly">
         <div class="col-sm-12 col-md-6 col-lg-3 col-xl-6 col-xxl-6">
         <ul class="list-unstyled d-flex flex-wrap flex-sm-row flex-md-column text-center text-lg-center justify-content-evenly align-items-center">
@@ -13,4 +13,148 @@
         <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">lorem</div>
         <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3">lorem</div>
     </div>
+</div> --}}
+
+<div>
+
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color: #1c2331"
+          >
+    <!-- Section: Social media -->
+    <section
+             class="d-flex justify-content-between p-4"
+             style="background-color: #6351ce"
+             >
+      <!-- Left -->
+      <div class="me-5">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+
+      <!-- Right -->
+      <div>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold">Company name</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              Here you can find infos about our company, feel free to contact us if you need informations about our transportation services.
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Products</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#!" class="text-white">Github</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Slack</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Google Drive</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Microsoft Account</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Useful links</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#" class="text-white">Your Account</a>
+            </p>
+            <p>
+              <a href="#" class="text-white">Become an Affiliate</a>
+            </p>
+            <p>
+              <a href="#" class="text-white">Shipping Rates</a>
+            </p>
+            <p>
+              <a href="#" class="text-white">Help</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Contact</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p><i class="fas fa-home mr-3"></i> Tresivio (SO), 23020</p>
+            <p><i class="fas fa-envelope mr-3"></i>simone.moretti.944@gmail.com</p>
+            <p><i class="fas fa-phone mr-3"></i> + 01 234 567</p>
+            <p><i class="fas fa-print mr-3"></i> + 01 234 567</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/"
+         >MDBootstrap.com</a
+        >
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+
 </div>
