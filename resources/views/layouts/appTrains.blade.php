1039b6884b34c75598e26381dfe6960c1443a5b5
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-27 14:22:02 +0200

    12.3th styling trains info responsive

--- resources/views/layouts/appTrains.blade.php
+++ resources/views/layouts/appTrains.blade.php
@@ -24,24 +24,31 @@
     <h2 id="country" class="text-white text-capitalize">{{$id}}</h2>
         <div class="row flex-column" id="country_row">
         @foreach ($trains as $train)
-            <div id="ticket_column" class="col-12 p-2 text-white d-flex border border-1 rounded-2 mt-2">
-                <div class="col-6 d-flex flex-column">
+            <div id="train_info_column" class="col-12 p-2 text-white d-flex border border-1 rounded-2 mt-2">
+
+            {{-- open train info left side --}}
+                <div id="train_info_left" class="col-6 d-flex flex-column">
                     <p class="col-12 text-primary">Operated by: <span class="text-white">{{$train['company']}}</span></p>
-                    <p class="col-12 text-primary text-smal">
+                    <p id="train_departure_arrival" class="col-12 text-primary text-smal">
                         Departure: <span class="text-white">{{$train['departure_station']}}</span> <span class="text-success">|</span>
                         Arrival: <span class="text-white">{{$train['arrival_station']}}</span>
                     </p>
                 </div>
-                <div class="col-6 d-flex justify-content-evenly  text-center">
-                    <div class="col-3 d-flex flex-column">
-                        <p class="col-12">{{$train['departure_station']}}</p>
+            {{-- close train info left side --}}
+            
+            {{-- open train info right side --}}
+                <div id="train_info_right" class="col-6 row justify-content-evenly  text-center">
+                    <div class="col d-flex flex-column">
+                        <p class="col-12"><span id="span_departure_arrival" class="text-primary">Departure: </span>{{$train['departure_station']}}</p>
                         <p class="col-12">{{$train['departure_time']}}</p>
                     </div>
-                    <div class="col-3 d-flex flex-column">
-                        <p class="col-12">{{$train['arrival_station']}}</p>
+                    <div class="col d-flex flex-column">
+                        <p class="col-12"><span id="span_departure_arrival" class="text-primary">Arrival: </span>{{$train['arrival_station']}}</p>
                         <p class="col-12">{{$train['arrival_time']}}</p>
                     </div>
                 </div>
+            {{-- close train info right side --}}
+
             </div>
         @endforeach
         </div>
