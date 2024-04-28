c0af335e14fa72d0d2e1bd63419a5b159113c445
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-28 14:27:27 +0200

    15.0.1th added favicon to index and all

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel="icon" type="images/x-icon" href="{{Vite::asset('resources/img/favicon_index.svg')}}" />
 
         <title>@yield('title' , 'app')</title>
 
